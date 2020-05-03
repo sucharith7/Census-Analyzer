15f2a19675c3f8ac8ce09aa5d17d8054c3da334c
sucharith7 <ch.sucharith09@gmail.com>
2020-05-03 21:54:33 +0530

    [sucharith] added test the given file path is correct or not

--- src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
+++ src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
@@ -1,18 +1,36 @@
 package com.censusanalyzertest;
 
 import com.censusanalyzer.CensusAnalyzer;
+import com.censusexception.CensusException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CensusAnalyzerTest {
 
     public final String CSV_FILE = "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\StateCensusData.csv";
+    public final String WRONG_FILE= "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\StateCensu.csv";
 
     @Test
     public void givenCSVFile_WhenMatchNumberOfRecords_ShouldReturnNumber() {
         CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
-        int numberOfStates = censusAnalyzer.loadCensusData(CSV_FILE);
+        int numberOfStates = 0;
+        try {
+            numberOfStates = censusAnalyzer.loadCensusData(CSV_FILE);
+        } catch (CensusException exception) {
+            exception.printStackTrace();
+        }
         Assert.assertEquals(29,numberOfStates);
+    }
+
+    @Test
+    public void givenCSVFile_WhenDoesNotExist_ShouldThrowException() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        int numberOfStates = 0;
+        try {
+            numberOfStates = censusAnalyzer.loadCensusData(WRONG_FILE);
+        } catch (CensusException exception) {
+            Assert.assertEquals(CensusException.exceptionType.CENSUS_FILE_ERROR,CensusException.exceptionType.CENSUS_FILE_ERROR);
+        }
     }
 }
 
