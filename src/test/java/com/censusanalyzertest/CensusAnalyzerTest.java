1c3f0f623e404aea5729fe1a94454b955e6a8a72
sucharith7 <ch.sucharith09@gmail.com>
2020-05-03 22:07:23 +0530

    [sucharith] added test delimter is correct

--- src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
+++ src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
@@ -10,6 +10,7 @@ public class CensusAnalyzerTest {
     public final String CSV_FILE = "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\StateCensusData.csv";
     public final String WRONG_FILE= "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\StateCensu.csv";
     public final String WRONG_TYPE = "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\StateCensusData.txt";
+    public final String DATA_FILE= "C:\\Users\\sucha\\IdeaProjects\\CensusAnalyzer\\statecensus.csv";
 
     @Test
     public void givenCSVFile_WhenMatchNumberOfRecords_ShouldReturnNumber() {
@@ -42,6 +43,17 @@ public class CensusAnalyzerTest {
             numberOfStates = censusAnalyzer.loadCensusData(WRONG_TYPE);
         } catch (CensusException exception) {
             Assert.assertEquals(CensusException.exceptionType.CENSUS_FILE_ERROR,CensusException.exceptionType.CENSUS_FILE_ERROR);
+        }
+    }
+
+    @Test
+    public void givenCSVFile_WhenImproperDelimeter_ShouldThrowException() {
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        int numberOfStates = 0;
+        try {
+            numberOfStates = censusAnalyzer.loadCensusData(DATA_FILE);
+        } catch (CensusException exception) {
+            Assert.assertEquals(CensusException.exceptionType.OTHER_FILE_ERROR,CensusException.exceptionType.OTHER_FILE_ERROR);
         }
     }
 }
