bcc4bdcca2d25adba4c67f574f5e333408945cb0
sucharith7 <ch.sucharith09@gmail.com>
2020-05-03 21:55:26 +0530

    [sucharith] added enum exception for census analyzer

--- src/main/java/com/censusexception/CensusException.java
+++ src/main/java/com/censusexception/CensusException.java
@@ -1,4 +1,15 @@
 package com.censusexception;
 
-public class CensusException {
+public class CensusException extends Exception{
+    public final exceptionType type ;
+
+    public enum exceptionType{
+        CENSUS_FILE_ERROR
+    };
+
+    public CensusException(exceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
 }
+
