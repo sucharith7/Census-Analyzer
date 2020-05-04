5f7b54ac13299fd857cf3f066c4e1f31ec01e5ef
sucharith7 <ch.sucharith09@gmail.com>
2020-05-04 10:50:59 +0530

    [sucharith] added code analyzer file

--- src/main/java/com/codeanalyzer/CodeAnalyzer.java
+++ src/main/java/com/codeanalyzer/CodeAnalyzer.java
@@ -1,18 +1,20 @@
 package com.codeanalyzer;
 
 import com.censusdata.CensusData;
+import com.censusexception.CensusException;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
 public class CodeAnalyzer {
 
-    public static int loadStateData(String csvFileState){
+    public static int loadStateData(String csvFileState) throws CensusException{
         int count = 0;
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFileState));
@@ -25,6 +27,8 @@ public class CodeAnalyzer {
                 count++;
                 CensusData csvUser = csvUserIterator.next();
             }
+        } catch (NoSuchFileException exception) {
+            throw new CensusException(CensusException.exceptionType.CENSUS_FILE_ERROR, "please enter proper file path or file type");
         } catch (IOException exception) {
             exception.printStackTrace();
         }
