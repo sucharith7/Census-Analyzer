b141b1d6808d5e69964a2ed80d8d05fb3bb2fa52
sucharith7 <ch.sucharith09@gmail.com>
2020-05-04 10:55:31 +0530

    [sucharith] added test the corerct type of file

--- src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
+++ src/test/java/com/censusanalyzertest/CensusAnalyzerTest.java
@@ -91,6 +91,17 @@ public class CensusAnalyzerTest {
             Assert.assertEquals(CensusException.exceptionType.CENSUS_FILE_ERROR,CensusException.exceptionType.CENSUS_FILE_ERROR);
         }
     }
+
+    @Test
+    public void givenStateCSVFile_WhenNotCorrectType_ShouldThrowException() {
+        CodeAnalyzer codeAnalyzer = new CodeAnalyzer();
+        int count = 0;
+        try {
+            count = codeAnalyzer.loadStateData(WRONG_TYPE);
+        } catch (CensusException exception) {
+            Assert.assertEquals(CensusException.exceptionType.CENSUS_FILE_ERROR,CensusException.exceptionType.CENSUS_FILE_ERROR);
+        }
+    }
 }
 
 
