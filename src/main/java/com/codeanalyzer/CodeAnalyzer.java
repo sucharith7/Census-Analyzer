35c1f8e6cbdb448a761ddd52bc8a70e9dc499ca8
sucharith7 <ch.sucharith09@gmail.com>
2020-05-04 10:38:54 +0530

    [sucharith] added code analyzer file for state code

--- src/main/java/com/codeanalyzer/CodeAnalyzer.java
+++ src/main/java/com/codeanalyzer/CodeAnalyzer.java
@@ -1,4 +1,33 @@
 package com.codeanalyzer;
 
+import com.censusdata.CensusData;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
 public class CodeAnalyzer {
+
+    public static int loadStateData(String csvFileState){
+        int count = 0;
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(csvFileState));
+            CsvToBean<CensusData> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(CensusData.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CensusData> csvUserIterator = csvToBean.iterator();
+            while (csvUserIterator.hasNext()) {
+                count++;
+                CensusData csvUser = csvUserIterator.next();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return count;
+    }
 }
