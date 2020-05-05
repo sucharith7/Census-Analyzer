d481fa7f3c8b4192226be07e115fd902cabfa374
sucharith7 <ch.sucharith09@gmail.com>
2020-05-05 14:43:18 +0530

    [sucharith] added csvbuilder file

--- src/main/java/com/opencsvbuilder/OpenCSVBuilder.java
+++ src/main/java/com/opencsvbuilder/OpenCSVBuilder.java
@@ -1,4 +1,22 @@
 package com.opencsvbuilder;
 
+import com.censusexception.CensusException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.Reader;
+import java.util.Iterator;
+
 public class OpenCSVBuilder {
+    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusException {
+        try {
+            CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(csvClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.iterator();
+        } catch (RuntimeException exception) {
+            throw new CensusException(CensusException.exceptionType.OTHER_FILE_ERROR, "please enter proper delimter  or proper header");
+        }
+    }
 }
