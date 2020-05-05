943ae35e5d76924bc7aa9c2b9d4ae913a0a9b52e
sucharith7 <ch.sucharith09@gmail.com>
2020-05-05 22:19:07 +0530

    [sucharith] added implenmented icsv builder in open csv builder

--- src/main/java/com/csvbuilderfactory/CSVBuilderFactory.java
+++ src/main/java/com/csvbuilderfactory/CSVBuilderFactory.java
@@ -1,4 +1,10 @@
 package com.csvbuilderfactory;
 
+import com.census.ICSVBuilder;
+import com.opencsvbuilder.OpenCSVBuilder;
+
 public class CSVBuilderFactory {
+    public static ICSVBuilder createCSVBuilder(){
+        return new OpenCSVBuilder();
+    }
 }
