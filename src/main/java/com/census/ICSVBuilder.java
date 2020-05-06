7662b06dcce9c07d49114ea1d233fa7c902a7513
sucharith7 <ch.sucharith09@gmail.com>
2020-05-06 16:35:01 +0530

    [sucharith] added csvlist to the icsvbuilder file

--- src/main/java/com/census/ICSVBuilder.java
+++ src/main/java/com/census/ICSVBuilder.java
@@ -4,7 +4,9 @@ import com.opencsvbuilder.CSVBuilderException;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader,Class csvClass) throws CSVBuilderException;
+    public  <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException;
+    public <E> List<E>  getCSVFileList(Reader reader,Class<E> csvClass) throws CSVBuilderException;
 }
