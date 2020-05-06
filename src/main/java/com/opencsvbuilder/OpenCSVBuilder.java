f6632912cab29299773588be34eda06b7d77dffb
sucharith7 <ch.sucharith09@gmail.com>
2020-05-06 16:36:17 +0530

    [sucharith] added csvlist to opencsv builder

--- src/main/java/com/opencsvbuilder/OpenCSVBuilder.java
+++ src/main/java/com/opencsvbuilder/OpenCSVBuilder.java
@@ -7,17 +7,25 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
-public class OpenCSVBuilder <E> implements ICSVBuilder {
-    public <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException {
+public class OpenCSVBuilder<E> implements ICSVBuilder {
+    public  <E>Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuilderException{
+        return this.getCSVBean(reader,csvClass).iterator();
+    }
+
+    @Override
+    public <E> List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws CSVBuilderException {
+        return this.getCSVBean(reader, csvClass).parse();
+    }
+    private <E> CsvToBean<E> getCSVBean(Reader reader,Class<E> csvClass) throws CSVBuilderException {
         try {
-            CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
-                    .withType(csvClass)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-            return csvToBean.iterator();
+            CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+            csvToBeanBuilder.withType(csvClass);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            return csvToBeanBuilder.build();
         } catch (RuntimeException exception) {
-            throw new CSVBuilderException("please enter proper delimeter or proper header",CensusException.exceptionType.OTHER_FILE_ERROR);
+            throw new CSVBuilderException("please enter proper delimeter or proper header", CensusException.exceptionType.OTHER_FILE_ERROR);
         }
     }
 }
