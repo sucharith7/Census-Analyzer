ff64aeb1bff35fed4b8fc339ba86fb5eaf460c1c
sucharith7 <ch.sucharith09@gmail.com>
2020-05-10 14:36:39 +0530

    [sucharith] added, state census DAO file

--- src/main/java/com/statecensusanalyzer/StateCensusDAO.java
+++ src/main/java/com/statecensusanalyzer/StateCensusDAO.java
@@ -1,4 +1,18 @@
 package com.statecensusanalyzer;
 
+import com.statecensus.StateCensus;
+
 public class StateCensusDAO {
+    public String state;
+    public String stateCode;
+    public int densityPerSqKm;
+    public int areaInSqKm;
+    public int population;
+
+    public StateCensusDAO(StateCensus stateCensus) {
+        state = stateCensus.state;
+        densityPerSqKm = stateCensus.densityPerSqKm;
+        areaInSqKm = stateCensus.areaInSqKm;
+        population = stateCensus.population;
+    }
 }
